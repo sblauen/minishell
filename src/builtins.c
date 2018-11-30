25742bb0fe1f703e693e5dc536f69e821591d38d
sblauens <sblauens@student.42.fr>
2018-11-30 05:47:10 +0100

    Cd: add PWD and OLDPWD update

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 01:49:51 by sblauens          #+#    #+#             */
-/*   Updated: 2018/11/30 03:52:28 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/11/30 05:36:07 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,34 @@
 #include <sys/stat.h>
 #include "minishell.h"
 
+static inline int		chpwd(char ***env)
+{
+	int					r;
+	char				*buf;
+	char				**args;
+
+	args = (char *[2]){"OLDPWD", _getenv("PWD", (const char **)*env)};
+	if (_setenv((const char **)args, env) == -1)
+		return (-1);
+	buf = getcwd(NULL, 0);
+	args = (char *[2]){"PWD", buf};
+	r = _setenv((const char **)args, env);
+	free(buf);
+	return (r);
+}
+
 /*
 **  Change the current working directory.
 */
 
-int						_builtin_cd(const char *path, const char **env)
+int						_builtin_cd(const char *path, char ***env)
 {
 	struct stat			sb;
 
 	if (!path)
-		path = _getenv("HOME", env);
+		path = _getenv("HOME", (const char **)*env);
 	if (!chdir(path))
-		return (0);
+		return (chpwd(env));
 	if (stat(path, &sb) == -1)
 		puterr("cd: no such file or directory: ", path);
 	else if ((sb.st_mode & S_IFMT) != S_IFDIR)
