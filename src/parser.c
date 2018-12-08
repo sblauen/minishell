df2385cf9759f5da6bb7b303ff7e1fda4f05d547
sblauens <sblauens@student.42.fr>
2018-12-08 22:18:46 +0100

    Add double quote parsing

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 13:32:27 by sblauens          #+#    #+#             */
-/*   Updated: 2018/12/08 21:42:11 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/12/08 21:49:58 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,17 @@ char					*quote_s(char *cp)
 	return (cp);
 }
 
+char					*quote_d(char *cp)
+{
+	while (*(cp + 1) != '"' && *(cp + 1))
+	{
+		*cp = *(cp + 1);
+		++cp;
+	}
+	ft_strcpy(cp, cp + 2);
+	return (cp);
+}
+
 char					*line_parse(char *line)
 {
 	char				*tmp;
@@ -33,6 +44,8 @@ char					*line_parse(char *line)
 	{
 		if (*tmp == '\'')
 			tmp = quote_s(tmp);
+		else if (*tmp == '"')
+			tmp = quote_d(tmp);
 		else
 			++tmp;
 	}
