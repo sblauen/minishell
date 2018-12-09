c043dc9c54a67e0a3e2cfd5b3c0c49387ae1150e
sblauens <sblauens@student.42.fr>
2018-12-10 00:10:07 +0100

    Add $ expansion

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 13:32:27 by sblauens          #+#    #+#             */
-/*   Updated: 2018/12/09 06:30:17 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/12/16 23:32:07 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,44 @@ char					*set_home(char *chrp, char **line, const char **env)
 	return (*line + retlen);
 }
 
+/*
+**  Modify the variable pointed to by 'chrp' with its value.
+**
+**  Returns a pointer to the character following the last
+**  one of the variable's value.
+**  The three values contained in 'len[3]' are respectively
+**  the variable, the value and the return length.
+*/
+
+static inline char		*set_var(char *chrp, char **line, const char **env)
+{
+	char				*tmp;
+	char				*val;
+	size_t				len[3];
+
+	len[0] = 0;
+	while (chrp + len[0] + 1 && ft_isalnum(chrp[len[0] + 1]))
+		++len[0];
+	if (!(tmp = (char *)malloc((len[0] + 1) * sizeof(char))))
+		return (chrp + 1);
+	ft_strlcpy(tmp, chrp + 1, len[0] + 1);
+	val = _getenv(tmp, env);
+	free(tmp);
+	if (val)
+	{
+		len[1] = ft_strlen(val);
+		if (!(tmp = (char *)malloc((ft_strlen(*line) + len[1]) * sizeof(char))))
+			return (chrp + 1);
+		len[2] = ft_strlcpy(tmp, *line, (chrp - *line) + 1);
+		len[2] = ft_strlcat(tmp, val, len[2] + len[1]);
+		ft_strcat(tmp, chrp + len[0] + 1);
+		free((void *)*line);
+		*line = tmp;
+		return (*line + len[2]);
+	}
+	return (chrp + 1);
+}
+
 char					*quote_s(char *cp)
 {
 	while (*(cp + 1) != '\'' && *(cp + 1))
@@ -47,8 +85,15 @@ char					*quote_d(char *cp, char **line, const char **env)
 {
 	while (*(cp + 1) != '"' && *(cp + 1))
 	{
-		*cp = *(cp + 1);
-		++cp;
+		if (*(cp + 1) == '$')
+		{
+			cp = set_var(cp, line, env);
+		}
+		else
+		{
+			*cp = *(cp + 1);
+			++cp;
+		}
 	}
 	ft_strcpy(cp, cp + 2);
 	return (cp);
@@ -67,6 +112,8 @@ char					*line_parse(char *line, const char **env)
 			tmp = quote_d(tmp, &line, env);
 		else if (*tmp == '~')
 			tmp = set_home(tmp, &line, env);
+		else if (*tmp == '$')
+			tmp = set_var(tmp, &line, env);
 		else
 			++tmp;
 	}
